53d98bc35477e93cf08bd24cabd05f707553f082
eliaslamanna <eliaslamanna@hotmail.com>
2022-04-04 01:41:15 -0300

    resource server

---
+++ Oauth-resource-server/src/main/java/com/resourceserver/Oauthresourceserver/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.resourceserver.Oauthresourceserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/api/users")
+    public String[] getUsers() {
+        return new String[] {
+          "Elias",
+          "Trini"
+        };
+    }
+
+}
